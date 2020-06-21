467653536454697c286983e60eeb2a302598c26a
clovisdanielcosta <58163677+clovisdanielcosta@users.noreply.github.com>
2020-06-21 01:42:27 -0300

    Adicionando exemplos com JOINS

--- auladb.sql
+++ auladb.sql
@@ -171,6 +171,63 @@ SELECT MIN(valor), tipo_transacao_id
 	FROM cliente_transacoes 
 	GROUP BY tipo_transacao_id;
 
+SELECT numero, nome FROM banco;
+SELECT banco_numero, numero, nome FROM agencia;
+SELECT numero, nome FROM cliente;
+SELECT banco_numero, agencia_numero, numero, digito, cliente_numero FROM conta_corrente;
+SELECT id, nome FROM tipo_transacao;
+SELECT banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, valor 
+	FROM cliente_transacoes;
+
+SELECT COUNT(1) FROM banco; -- 151
+SELECT COUNT(1) FROM agencia; -- 296
+
+-- JOIN
+
+-- 296
+SELECT banco.numero, banco.nome, agencia.numero, agencia.nome
+	FROM banco
+	JOIN agencia ON agencia.banco_numero = banco.numero;
+
+-- Qtos bancos têm agências = 9
+SELECT COUNT(distinct banco.numero)
+	FROM banco
+	JOIN agencia ON agencia.banco_numero = banco.numero;
+
+-- LEFT JOIN
+SELECT banco.numero, banco.nome, agencia.numero, agencia.nome 
+	FROM banco
+	LEFT JOIN agencia ON agencia.banco_numero = banco.numero;
+
+-- RIGHT JOIN
+SELECT agencia.numero, agencia.nome, banco.numero, banco.nome 
+	FROM agencia
+	RIGHT JOIN banco ON banco.numero = agencia.banco_numero;
+
+-- FULL JOIN
+SELECT banco.numero, banco.nome, agencia.numero, agencia.nome 
+	FROM banco
+	FULL JOIN agencia ON agencia.banco_numero = banco.numero;
+
+-- CROSS JOIN
+SELECT bc.numero, bc.nome, ag.numero, ag.nome 
+	FROM banco bc -- Criando alias para banco chamado bc
+	CROSS JOIN agencia ag; --Criando alias para banco chamado bc
+	
+-- JOIN COM MAIS TABELAS
+SELECT 	banco.nome,
+		agencia.nome,
+		conta_corrente.numero,
+		conta_corrente.digito,
+		cliente.nome
+FROM 	banco
+JOIN 	agencia 
+	ON 	agencia.banco_numero = banco.numero
+JOIN	conta_corrente
+	ON	conta_corrente.banco_numero = banco.numero
+	AND	conta_corrente.agencia_numero = agencia_numero
+JOIN	cliente
+	ON	cliente.numero = conta_corrente. cliente_numero;
 
 
 
